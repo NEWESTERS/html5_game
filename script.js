46249afe3daf5a82709d2484b21ee5046cb398e8
NEWESTERS <ramiro.cruz@mail.ru>
2018-05-05 12:12:21 +0300

    Score add

--- script.js
+++ script.js
@@ -40,6 +40,9 @@ function Brick (ctx, x, y, width, height) {
 	this.mark = function() {
 		if (this.isCollided) {
 			this.isMarked = true
+			return true
+		} else {
+			return false
 		}
 	}
 }).call(Brick.prototype)
@@ -47,7 +50,7 @@ function Brick (ctx, x, y, width, height) {
 function Player(ctx, new_x, new_y) {
 	GameObject.apply(this, arguments)
 	// Player fields
-	this._speed = 10,
+	this.speed = 10,
 	this._frameNumber = 0,
 	this._image = new Image()
 	this._image.src = 'player.png'
@@ -71,19 +74,6 @@ function Player(ctx, new_x, new_y) {
 	}
 	// Draw player
 	this._draw = function() {
-		/*
-		this._ctx.beginPath()
-		this._ctx.rect(
-			this.x,
-			this.y,
-			this.width,
-			this.height
-		)
-		this._ctx.fillStyle = "#000099"
-		
-		this._ctx.fill()
-		this._ctx.closePath()
-		*/
 		let sx = 0, sy = 0
 
 		if (this.direction == 'left') sy = 410
@@ -111,19 +101,19 @@ function Player(ctx, new_x, new_y) {
     this.move = function(collision) {
 		if (this.state == 'walk') {
 			if (this.direction == 'left' && this.x >= 0 && !collision.left) {
-				this.x -= this._speed
+				this.x -= this.speed
 			} else if (this.direction == 'right' && this.x <= GAME_WIDTH - this.width && !collision.right) {
-				this.x += this._speed
+				this.x += this.speed
 			} else if (this.direction == 'up' && this.y >= 0 && !collision.up) {
-				this.y -= this._speed
+				this.y -= this.speed
 				// Perspective simulation
-				//this._speed -= .1
+				//this.speed -= .1
 				//this.height -= .75
 				this.width = this.height / 1.4
 			} else if (this.direction == 'down' && this.y <= GAME_HEIGHT - this.height && !collision.down) {
-				this.y += this._speed
+				this.y += this.speed
 				// Perspective simulation
-				//this._speed += .1
+				//this.speed += .1
 				//this.height += .75
 				this.width = this.height / 1.4
 			}
@@ -142,29 +132,45 @@ function detectCollision(objA, objB) {
 		down: false
 	}
 
-	if (!(objA.y - objA._speed < objB.y && objA.y + objA.height - objA._speed < objB.y) &&
-		!(objA.y + objA._speed > objB.y + objB.height && objA.y + objA._speed + objA.height > objB.y + objB.height)) {
-		if (objA.x - objA._speed < objB.x + objB.width && objA.x + objA._speed > objB.x && objA) {
+	if (!(objA.y - objA.speed < objB.y && objA.y + objA.height - objA.speed < objB.y) &&
+		!(objA.y + objA.speed > objB.y + objB.height && objA.y + objA.speed + objA.height > objB.y + objB.height)) {
+		if (objA.x - objA.speed < objB.x + objB.width && objA.x + objA.speed > objB.x && objA) {
 			collision.left = true
 		}
 
-		if (objA.x + objA.width + objA._speed > objB.x && objA.x - objA._speed < objB.x) {
+		if (objA.x + objA.width + objA.speed > objB.x && objA.x - objA.speed < objB.x) {
 			collision.right = true
 		}
 	}
 
-	if (!(objA.x - objA._speed < objB.x && objA.x + objA.width - objA._speed < objB.x) &&
-		!(objA.x + objA._speed > objB.x + objB.width && objA.x + objA.width + objA._speed > objB.x + objB.width)) {
-		if (objA.y - objA._speed < objB.y + objB.height && objA.y + objA._speed > objB.y) {
+	if (!(objA.x - objA.speed < objB.x && objA.x + objA.width - objA.speed < objB.x) &&
+		!(objA.x + objA.speed > objB.x + objB.width && objA.x + objA.width + objA.speed > objB.x + objB.width)) {
+		if (objA.y - objA.speed < objB.y + objB.height && objA.y + objA.speed > objB.y) {
 			collision.up = true
 		}
 
-		if (objA.height + objA.y + objA._speed > objB.y && objA.y - objA._speed < objB.y) {
+		if (objA.height + objA.y + objA.speed > objB.y && objA.y - objA.speed < objB.y) {
 			collision.down = true
 		}
 	}
 
 	return collision
+}
+
+function count_score(n) {
+	if (n != 0) {
+		let a = 1,
+	    	b = 1
+	    	c = 0
+		for (let i = 3; i <= n; i++) {
+	    	c = a + b
+	    	a = b
+	    	b = c + 1
+	  	}
+	  	return b
+	} else {
+		return 0
+	}
 }
 
 window.onload = _ => {
@@ -173,6 +179,8 @@ window.onload = _ => {
 	GAME_WIDTH = canvas.width
 	GAME_HEIGHT = canvas.height
 
+	let score = 0
+	let marked = 0
 	const player = new Player(ctx, canvas.width / 2, canvas.height / 2)
 
 	let objects = []
@@ -188,12 +196,6 @@ window.onload = _ => {
 		height = Math.random() * 200
 		objects.push(new Brick(ctx, x, y, width, height))
 	}
-	/*
-	let objects = [new Brick(ctx, 195, 200, 200, 50),
-				   new Brick(ctx, 693, 400, 50, 50),
-				   new Brick(ctx, 693, 400, 50, 50)
-	]
-	*/
 
 	document.addEventListener("keydown", e => {
 		if(e.keyCode == 37) {
@@ -210,10 +212,17 @@ window.onload = _ => {
 	    	player.state = 'walk'
 	    } else if (e.keyCode == 32) {
 	    	for (var i = objects.length - 1; i >= 0; i--) {
-	    		if (objects[i].isMarked && objects[i].isCollided) {
-	    			objects.splice(i, 1)
+	    		if (objects[i].mark()) {
+	    			marked++
 	    		}
-	    		objects[i].mark()
+	    	}
+	    } else if (e.keyCode == 13) {
+	    	score += count_score(marked)
+	    	for (var i = objects.length - 1; i >= 0; i--) {
+	    		if (objects[i].isMarked) {
+	    			objects.splice(i, 1)
+	    			marked--
+	    		}
 	    	}
 	    }
 	})
@@ -249,12 +258,15 @@ window.onload = _ => {
 			finalCollision.down = (finalCollision.down + collision.down) ? true : false
 		}
 
-		//objects[0].draw()
-		//objects[1].draw()
 		for (let i = objects.length - 1; i >= 0; i--) {
 			objects[i].draw()
 		}
 		player.move(finalCollision)
+
+		ctx.font = "20px Arial"
+		ctx.fillStyle = "red"
+		ctx.fillText(`Score: ${score}`, 50, 50)
+
 		requestAnimationFrame(update)
 	})()
 }
